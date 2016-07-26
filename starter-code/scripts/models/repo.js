1b2e1c11a96b041bf57ac9fce03a35aa9fa2e984
David Smith <dbsmith.dbs83@gmail.com>
2016-07-26 15:43:47 -0700

    got data, stored in obj, put in html dependencies

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,6 +6,19 @@
   reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/codefellows-seattle-301d9/repos' +
+         '?per_page=1' +
+         '&sort=update',
+      type: 'GET',
+      headers: {
+        'Authorization': 'token ' + githubToken,
+      },
+      success: function(data, message, xhr) {
+        reposObj.allRepos = data;
+        console.log(data);
+      }
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
