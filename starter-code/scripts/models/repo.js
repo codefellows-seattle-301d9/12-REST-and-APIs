630b8927ee827c4aa82a0a087b5893465f8a8191
aziza alhouni <azizaalhouni@yahoo.com>
2016-07-26 15:16:45 -0700

    first4

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -4,8 +4,20 @@
   reposObj.allRepos = [];
 
   reposObj.requestRepos = function(callback) {
-    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
+    /* TODO: Done! How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/codefellows-seattle-301d9/repos',
+      type: 'GET',
+      headers: {
+        'Authorization':'token' + token,
+      },
+      success: function(data, message, xhr) {
+        console.log(data);
+      }
+// new messages
+
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
